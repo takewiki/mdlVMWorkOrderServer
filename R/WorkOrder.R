2aeb6594db422df18cfeaba75c71b0e699668e3a
takewiki <hulilei@takeiwki.com.cn>
2025-12-02 10:12:20 +0000

    v1.0.0

---
+++ R/WorkOrder.R
@@ -0,0 +1,65 @@
+
+#' 处理逻辑
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param session 会话
+#' @param dms_token 口令
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' WorkOrderSelectServer()
+WorkOrderSelectServer <- function(input,output,session,dms_token) {
+  #获取参数
+  text_WorkOrder=tsui::var_text('text_WorkOrder')
+
+
+  #查询按钮
+
+  shiny::observeEvent(input$btn_WorkOrder_view,{
+
+    FWorkOrder=text_WorkOrder()
+
+
+    if(FWorkOrder==''){
+
+      tsui::pop_notice("Please Enter Work Order")
+
+
+    }else{
+      data = mdlVmWorkOrderr::WorkOrder_selectByDate(dms_token = dms_token,FCalculateYear = FCalculateYear,FLatestVersion =FLatestVersion ,FVersion = FVersion,FCalculationPeriod = FCalculationPeriod)
+
+      tsui::run_dataTable2(id ='WorkOrder_resultView' ,data =data )
+
+      tsui::run_download_xlsx(id = 'dl_WorkOrder',data = data,filename = 'WorkOrder.xlsx')
+
+
+    }
+
+
+  })
+
+
+
+}
+
+
+#' 处理逻辑
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param session 会话
+#' @param dms_token 口令
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' WorkOrderServer()
+WorkOrderServer <- function(input,output,session,dms_token) {
+  WorkOrderSelectServer(input = input,output = output,session = session,dms_token = dms_token)
+
+
+}
