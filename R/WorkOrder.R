6f0c19c31cd9bc807fefb23ea20371d900cab01c
takewiki <hulilei@takeiwki.com.cn>
2025-12-15 02:08:53 +0000

    v1.0.3

--- R/WorkOrder.R
+++ R/WorkOrder.R
@@ -13,8 +13,74 @@
 #' @examples
 #' WorkOrderSelectServer()
 WorkOrderSelectServer <- function(input,output,session,app_id, run_env = "PRD") {
+  # 获取所有字段名
+  WorkOrder_all_columns <- c(
+    'Work OrderID',
+    'Work OrderId2',
+    'Production Date',
+    'Sales OrderID',
+    'Delivery Location',
+    'Sales OrderID2',
+    'PN',
+    'Product Name',
+    'Serial Number',
+    'Sales OrderQty',
+    'Delivery Date',
+    'Total DeliveryQty'
+  )
+
+  #设置默认值
+  WorkOrder_default_columns <- c(
+    'Work OrderID',
+    'PN',
+    'Product Name',
+    'Serial Number',
+    'Production Date',
+    'Sales OrderID',
+    'Delivery Location'
+  )
+  WorkOrder_reset_columns <- c(
+    'Work OrderID',
+    'Serial Number'
+  )
+
+  # 全选按钮
+  observeEvent(input$btn_WorkOrder_select_all, {
+    updatePickerInput(
+      session = session,
+      inputId = "WorkOrder_column_selector",
+      selected = WorkOrder_all_columns
+    )
+  })
+
+  # 取消全选按钮
+  observeEvent(input$btn_WorkOrder_deselect_all, {
+    updatePickerInput(
+      session = session,
+      inputId = "WorkOrder_column_selector",
+      selected = WorkOrder_reset_columns
+    )
+  })
+  # 默认值按钮
+  observeEvent(input$btn_WorkOrder_defaultValue, {
+    updatePickerInput(
+      session = session,
+      inputId = "WorkOrder_column_selector",
+      selected = WorkOrder_default_columns
+    )
+  })
   #获取参数
   text_WorkOrder=tsui::var_text('text_WorkOrder')
+  #查询按钮
+  # 显示选择信息
+  output$WorkOrder_selection_info <- renderPrint({
+    selected <- input$WorkOrder_column_selector
+    cat("Column Count: ", length(selected), "\n")
+    cat("Column List: ", paste(selected, collapse = ", "), "\n")
+    FOrderNumber=text_WorkOrder()
+    cat("Work Order Number:",FOrderNumber)
+  })
+
 
 
   #查询按钮
@@ -35,10 +101,12 @@ WorkOrderSelectServer <- function(input,output,session,app_id, run_env = "PRD") 
 
 
       data = mdlVMWorkOrderPkg::WorkOrder_select(erp_token = erp_token,FWorkOrderID = FWorkOrder)
-      # 增加对英文支持
-      tsui::run_dataTable2(id ='WorkOrder_resultView' ,data =data,lang='en' )
 
-      tsui::run_download_xlsx(id = 'dl_WorkOrder',data = data,filename = 'WorkOrder.xlsx')
+      data_selected = data[ ,input$WorkOrder_column_selector,drop=FALSE]
+      # 增加对英文支持
+      tsui::run_dataTable2(id ='WorkOrder_resultView' ,data =data_selected,lang='en' )
+
+      tsui::run_download_xlsx(id = 'dl_WorkOrder',data = data_selected,filename = 'WorkOrder.xlsx')
 
 
     }
